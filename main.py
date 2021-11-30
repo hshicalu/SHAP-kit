913e3cad2c59fabaeb53db37e1e48cf99dbc921e
hicalu <hshicalu@gmail.com>
2021-11-30 10:47:55 +0900

    update SHAP params

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ import pandas as pd
 import lightgbm as lgb
 import optuna
 from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import RobustScaler
 from sklearn.metrics import mean_absolute_error, mean_squared_error, mean_squared_log_error
 
 from preprocess import preprocess
@@ -42,6 +43,11 @@ if __name__ == '__main__':
     test_data = test.values
     X_test = test_data[:, 1:]
 
+    RS = RobustScaler()
+    RS.fit(X)
+    X = RS.transform(X)
+    X_test = RS.transform(X_test)
+
     X_train, X_valid, y_train, y_valid = train_test_split(X, y, test_size=.2, random_state=0)
     lgb_train = lgb.Dataset(X_train, y_train, free_raw_data=False)
     lgb_eval = lgb.Dataset(X_valid, y_valid, reference=lgb_train, free_raw_data=False)
@@ -64,5 +70,5 @@ if __name__ == '__main__':
     # (same syntax works for LightGBM, CatBoost, scikit-learn, transformers, Spark, etc.)
     shap.initjs()
     explainer = shap.TreeExplainer(model=gbm, data=X_train)
-    shap_values = explainer.shap_values(X_train)
+    shap_values = explainer.shap_values(X_train, check_additivity=False)
     shap.summary_plot(shap_values, features=X_train, plot_type="bar")
